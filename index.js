1fd8b71c38da46ed3640195814dfc095ca3e1c33
shish-ko <shishko.dev@gamil.com>
2024-05-24 01:14:14 +0300

    fix: cors2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import proxy from 'express-http-proxy'; 
+import proxy from 'express-http-proxy';
 import dotenv from 'dotenv';
 import cors from 'cors';
 dotenv.config();
@@ -8,21 +8,21 @@ const port = 3000;
 const corsOptions = {
   origin: 'http://localhost:5173/',
 };
-// https://image.tmdb.org/t/p/original/${req.params.id}
-const app=express();
+const app = express();
 
-app.get('/media/:id', (req, res)=> proxy('https://image.tmdb.org', {
-  proxyReqPathResolver: ()=>`/t/p/original/${req.params.id}`
+app.get('/media/:id', (req, res) => proxy('https://image.tmdb.org', {
+  proxyReqPathResolver: () => `/t/p/original/${req.params.id}`
 })(req, res));
-app.use(cors());
-app.use(proxy('https://api.themoviedb.org', {
-  
+app.get('/proxy/*', cors(corsOptions), proxy('https://api.themoviedb.org', {
+
   proxyReqOptDecorator: (proxyReqOpts) => {
     proxyReqOpts.headers['Authorization'] = process.env.API_KEY
     return proxyReqOpts;
+  },
+  proxyReqPathResolver: (req) => {
+    return req.url.replace('/proxy', '')
   }
-  
+
 }))
-// app.get('/search', (req, res)=> res(proxy(`https://image.tmdb.org/t/p/original/${req.params.id}`)))
 
 app.listen(port);
